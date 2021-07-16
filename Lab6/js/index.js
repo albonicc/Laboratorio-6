500df9d1987d8a94de54e194d79936688e103378
albonicc <nikygam@gmail.com>
2021-07-15 22:05:01 -0500

    Added validation when input is empty

--- Lab6/js/index.js
+++ Lab6/js/index.js
@@ -3,11 +3,15 @@ const list = $(".Lista");
 
 addItem.on("click", function() {
     let item = $("#newText").val();
-    list.append(`<div class="lis">
-                    <li>
-                    <p>${item}</p>
-                    <button class="checar">check</button>
-                    <button class="del">delete</button>
-                    </li>
-                </div>`)
+    if(item === "") {
+        alert("This field can't be empty!!!!");
+    } else {
+        list.append(`<div class="lis">
+                        <li>
+                        <p>${item}</p>
+                        <button class="checar">check</button>
+                        <button class="del">delete</button>
+                        </li>
+                    </div>`)
+    }
 })
