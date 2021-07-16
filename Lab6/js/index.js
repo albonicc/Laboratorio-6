c42e9003e53057e80222dc845c8f75f0c4f3457a
albonicc <nikygam@gmail.com>
2021-07-15 22:47:49 -0500

    Lab is finished

--- Lab6/js/index.js
+++ Lab6/js/index.js
@@ -1,17 +1,22 @@
 const addItem = $(".agregar");
 const list = $(".Lista");
 
+
 addItem.on("click", function() {
     let item = $("#newText").val();
     if(item === "") {
         alert("This field can't be empty!!!!");
     } else {
         list.append(`<div class="lis">
-                        <li>
-                        <p>${item}</p>
+                        <li class="li-item">
+                        <p class="list-item">${item}</p>
                         <button class="checar">check</button>
                         <button class="del">delete</button>
                         </li>
                     </div>`)
     }
+    $(".checar").click("button",function() {$(this).closest(".li-item").children(".list-item").toggleClass("chec")})
+    $(".del").click("button",function() {$(this).closest(".lis").remove()})
 })
+
+
